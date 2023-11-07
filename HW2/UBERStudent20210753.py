344bed4ea0c0e16cb0804be10baae7dc0f71a2d9
mk-star <ssunn0812@naver.com>
2023-11-07 12:34:04 +0900

    HW2 첫 번째

---
+++ HW2/UBERStudent20210753.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import calendar
+with open("uber_exp.txt", "r") as fr:
+    dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    while True:
+        row = fr.readline()
+        if not row:
+            break
+
+        data = row.split(",")
+        date = data[1].split("/")
+        day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+        with open("uberoutput.txt", "at") as fw:
+            fw.write("%s,%s %s,%s" % (data[0], dayofweek[day], data[2], data[3]))
