93ebe4e1e92cf336a021582091da63fc9ce622ea
mk-star <ssunn0812@naver.com>
2023-11-08 12:09:29 +0900

    UBER 두 번째

--- HW2/UBERStudent20210753.py
+++ HW2/UBERStudent20210753.py
@@ -3,6 +3,7 @@
 import calendar
 with open("uber.dat", "r") as fr:
     dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    result = dict()
     while True:
         row = fr.readline()
         if not row:
@@ -10,6 +11,16 @@ with open("uber.dat", "r") as fr:
 
         data = row.split(",")
         date = data[1].split("/")
-        day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
-        with open("uberoutput.txt", "at") as fw:
-            fw.write("%s,%s %s,%s" % (data[0], dayofweek[day], data[2], data[3]))
+        day = dayofweek[calendar.weekday(int(date[2]), int(date[0]), int(date[1]))]
+
+        if day not in result:
+            result[day] = []
+        else:
+            result[day].append((data[0], data[2], data[3]))
+for day in dayofweek:
+    result[day].sort(key=lambda x: x[0])
+
+with open("uberoutput.txt", "wt") as fw:
+    for day in dayofweek:
+        for r in result[day]:
+            fw.write("%s,%s %s,%s" % (r[0], day, r[1], r[2]))
